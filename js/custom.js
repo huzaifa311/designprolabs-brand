7ed605c8f34f1105d68ca0331414efc087a2283c
huzaifa311 <huzaifahabib311@gmail.com>
2024-10-17 21:19:34 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+new WOW().init();
+
+  
+$('#carouselFade').carousel();
+
+
+ 
+  $("#clients").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 5,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+      itemsDesktopSmall : [900,3], // betweem 900px and 601px
+      itemsTablet: [600,2],
+      itemsMobile : [320,2],
+      transitionStyle:"fade"
+      
+  });
+
+        
+
+	
+
+
+
+ $(document).ready(function() {
+  $(".fancybox").fancybox();
+
+ });
+
+
+
+
+
+
+
+// -------------------------------------------------------------
+    // ScrollBar Start
+    // -------------------------------------------------------------
+
+jQuery(document).ready(function(){
+    jQuery('.scrollbar-inner').scrollbar();
+});
+// -------------------------------------------------------------
+    // ScrollBar End
+    // -------------------------------------------------------------
+
+
+
+
+
