670886cc18908cae7e0c7c5b00d0f2a240f34fd4
huzaifa311 <huzaifahabib311@gmail.com>
2024-10-23 18:25:55 +0500

    web brief form added

---
+++ js/web-brief.js
@@ -0,0 +1,6 @@
+function setPrimaryColor(colorCode) {
+    document.getElementById('primarycolor').value = colorCode;
+}
+function setSecondaryColor(colorCode) {
+    document.getElementById('secondarycolor').value = colorCode;
+}
