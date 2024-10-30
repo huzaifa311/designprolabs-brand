f12ed2a7ee76de829d14de11a9d45b3586471f4e
huzaifa311 <huzaifahabib311@gmail.com>
2024-10-30 23:58:13 +0500

    update

--- js/custom.js
+++ js/custom.js
@@ -1,67 +1,65 @@
-
-
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
 });
 
 new WOW().init();
 
-
-$('#carouselFade').carousel();
-
-
+$("#carouselFade").carousel();
 
 $("#clients").owlCarousel({
+  autoPlay: 3000, //Set AutoPlay to 3 seconds
 
-    autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-    items: 5,
-    itemsDesktop: [1199, 3],
-    itemsDesktopSmall: [979, 3],
-    itemsDesktopSmall: [900, 3], // betweem 900px and 601px
-    itemsTablet: [600, 2],
-    itemsMobile: [320, 2],
-    transitionStyle: "fade"
-
+  items: 5,
+  itemsDesktop: [1199, 3],
+  itemsDesktopSmall: [979, 3],
+  itemsDesktopSmall: [900, 3], // betweem 900px and 601px
+  itemsTablet: [600, 2],
+  itemsMobile: [320, 2],
+  transitionStyle: "fade",
 });
-
-
-
-
-
-
 
 $(document).ready(function () {
-    $(".fancybox").fancybox();
-
+  $(".fancybox").fancybox();
 });
-
-
-
-
-
-
 
 // -------------------------------------------------------------
 // ScrollBar Start
 // -------------------------------------------------------------
 
 jQuery(document).ready(function () {
-    jQuery('.scrollbar-inner').scrollbar();
+  jQuery(".scrollbar-inner").scrollbar();
 });
 // -------------------------------------------------------------
 // ScrollBar End
 // -------------------------------------------------------------
 
-
-
-
-
 let values = ["Home", "👋 Hi there! Looking for a website?"];
 let index = 0;
 
 setInterval(() => {
-    document.title = values[index];
+  document.title = values[index];
 
-    index = (index + 1) % values.length;
+  index = (index + 1) % values.length;
 }, 2000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleActiveClass() {
+    const dropdowns = document.querySelectorAll("header .dropdown");
+
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth >= 0 && viewportWidth <= 500) {
+      dropdowns.forEach(function (dropdown) {
+        dropdown.classList.add("active");
+      });
+    } else {
+      dropdowns.forEach(function (dropdown) {
+        dropdown.classList.remove("active");
+      });
+    }
+  }
+
+  toggleActiveClass();
+
+  window.addEventListener("resize", toggleActiveClass);
+});
