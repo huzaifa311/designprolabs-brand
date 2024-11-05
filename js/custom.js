8e55289f9dd24b012d75f8d1a6bdae995453c473
huzaifa311 <huzaifahabib311@gmail.com>
2024-11-05 22:24:59 +0500

    hgSD

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
+var Tawk_API = Tawk_API || {},
+Tawk_LoadStart = new Date();
+(function () {
+var s1 = document.createElement("script"),
+  s0 = document.getElementsByTagName("script")[0];
+s1.async = true;
+s1.src = "https://embed.tawk.to/67293ec34304e3196add1144/1ibshqbi7";
+s1.charset = "UTF-8";
+s1.setAttribute("crossorigin", "*");
+s0.parentNode.insertBefore(s1, s0);
+})();
+
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
@@ -88,3 +100,12 @@ window.addEventListener("click", function (event) {
     modal.style.display = "none";
   }
 });
+
+
+function openTawkChat() {
+  if (typeof Tawk_API !== "undefined") {
+    Tawk_API.toggle();
+  } else {
+    console.error("Tawk.to API is not loaded yet.");
+  }
+}
