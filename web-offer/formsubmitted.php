3805625600505008ba185596ab363c1559db6ecd
huzaifa311 <huzaifahabib311@gmail.com>
2025-01-20 21:15:58 +0500

    update

---
+++ web-offer/formsubmitted.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-524K75WW');</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16824780855"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16824780855');
+</script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Submitted Successfully</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-524K75WW"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <div class="absolute left-3 top-3">
+        <a href="./index">
+        <img
+                src="assets/images/logo.png"
+                alt="Design Pro Labs"
+                class="md:ml-8 h-28"
+              />
+        </a>
+    </div>
+    <div class="min-h-screen w-full flex justify-center items-center">
+        <div class="w-[80%] h-full">
+            <img src="assets/images/thankyou.png" alt="Form Submitted" class="mx-auto">
+            <h1 class="text-center text-3xl font-bold mt-2">Thank you!</h1>
+            <p class="text-center text-gray-500 mt-2">Your Submission has been saved</p>
+        </div>
+    </div>
+</body>
+</html>
--- web-offer/include/header.php
+++ web-offer/include/header.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WDTKD2RG');</script>
-<!-- End Google Tag Manager -->
+})(window,document,'script','dataLayer','GTM-524K75WW');</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16824780855"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16824780855');
+</script>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,17 +24,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     />
     <?php include 'include/css.php'; ?>
     <title><?php echo isset($title) ? $title : 'Home'; ?></title>
-    <meta
-      name="google-site-verification"
-      content="eoQvGFbRf4tINHldCoPIUiDqaD8icW2ghKB_3s__4Og"
-    />
   </head>
-
   <body class="hompg">
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDTKD2RG"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-524K75WW"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- header -->
 
     <header class="header123">
